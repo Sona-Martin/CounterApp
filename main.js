4a09eba69dfd4f44beb5842b45e052959d89e334
Sona-Martin <sonarosemartin@gmail.com>
2022-01-18 00:22:06 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var n=document.getElementById("number"),
+count=0;
+function increment(){
+    count++;
+    n.innerHTML = count;
+};
+function decrement(){
+    count--;
+    n.innerHTML = count;
+};
